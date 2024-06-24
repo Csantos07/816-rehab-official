eceb5b4cd30a9f499af8c600bbe970a78c8a3217
Carlo Santos <rcarlosantos89@gmail.com>
2024-06-24 12:29:30 -0500

    base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
 
 export default App;
 
-{/*
+{
+  /*
   <img src='/logos/mobile-dark-bg.png' />
   <img src='/logos/eight-one-six-txt-logo.png' />
   <img src='/logos/mobile-splash-logo.png' />
-*/}
+*/
+}

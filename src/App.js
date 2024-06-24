af367e85d9ef9b8aca166edb5451480f442847fd
Carlo Santos <rcarlosantos89@gmail.com>
2024-06-24 11:29:01 -0500

    rearrange-images

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
     </div>
   );
 }
 
 export default App;
+
+{/*
+  <img src='/logos/mobile-dark-bg.png' />
+  <img src='/logos/eight-one-six-txt-logo.png' />
+  <img src='/logos/mobile-splash-logo.png' />
+*/}

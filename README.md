73c04c86098c3b40c8afa89dd4943b5a312422f2
Carlo Santos <rcarlosantos89@gmail.com>
2024-06-24 20:47:05 -0500

    first iteration mobile navbar started main

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+Next Steps. Extract css to mirror components.
 
+Main Section...
+Logo and button
+
+Use More HQ images
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Header = () => {
-  return <header className="header"></header>;
+  return (
+    <header className="header">
+      <img className="nav-logo" src="/logos/enlarged-logo.png" alt="" />
+      <div className="hamburger" id="hamburger">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
